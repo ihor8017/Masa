5abd31a4f0fb5d97d00c0101b31a611cdd721bdc
Ihor Chyzh <djar80@gmail.com>
2023-09-24 22:46:21 -0500

    desktop header

--- source/js/main.js
+++ source/js/main.js
@@ -10,7 +10,66 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  const menu = () => {
+    const navigation = document.querySelector('.header__navigation');
+    if (navigation) {
+      const button = document.querySelector('.navigation__button');
+      const menuList = document.querySelector('.navigation__list');
+      let height = menuList.scrollHeight;
+      console.log(height);
+      let overlay = document.querySelector('.overlay');
+      function closeMenu() {
+        navigation.classList.remove('navigation--open');
+        navigation.classList.add('navigation--close');
+        menuList.style.minHeight = '0';
+        menuList.style.width = '0';
+        menuList.style.padding = '0';
+        overlay.style.background = 'none';
+        document.body.style.overflow = 'visible';
+        document.body.style.overflowX = 'hidden';
+      }
+      function openMenu() {
+        navigation.classList.add('navigation--open');
+        navigation.classList.remove('navigation--close');
+        overlay.style.background = 'rgba(0, 0, 0, 0.5)';
+        menuList.style.minHeight = 'min-content';
+        menuList.style.width = '100%';
+        menuList.style.padding = '33px 25px';
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+      }
+      function onExternClick(evt) {
+        if (!evt.target.closest('.header__navigation')) {
+          document.removeEventListener('click', onExternClick, true);
+          closeMenu();
+        }
+      }
 
+      const toggleNavigation = () => {
+        navigation.classList.remove('no-js');
+        button.addEventListener('click', (evt) => {
+          evt.preventDefault();
+          if (navigation.classList.contains('navigation--open')) {
+            closeMenu();
+          } else {
+            document.addEventListener('click', onExternClick);
+            openMenu();
+          }
+        });
+        const navigationList = document.querySelector('.navigation__list');
+        navigationList.addEventListener('click', (evt) => {
+          const target = evt.target;
+          if (target.closest('.navigation__link')) {
+            closeMenu();
+          }
+        });
+      };
+      toggleNavigation();
+    } else {
+      return;
+    }
+  };
+  menu();
   // Modules
   // ---------------------------------
 
